417cecf6989070abb693a49e23ef006349c6e1f7
gh-actions <actions@github.com>
2021-11-18 08:09:54 +0000

    Deploy website - based on 2746cc9c3443757dcf7b695bcf24f24bc6e868d3

--- zh/assets/js/bfb16af5.17e87970.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkkubevela_io=self.webpackChunkkubevela_io||[]).push([[5909],{25281:function(e){"use strict";e.exports=JSON.parse('{"kubevela":{"allTagsPath":"/zh/blog/tags","slug":"kubevela","name":"kubevela","count":4,"permalink":"/zh/blog/tags/kubevela"}}')}}]);
